11828b02e1952b60ea565f1be1b2813e4cb52813
noodleincup <kkukkong102544@gmail.com>
2025-06-07 18:31:44 +0700

    Update for checking numpy version (for this tensorflow must run numpy as numpy less than version 2)

--- test_tensorflow.py
+++ test_tensorflow.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
-
+import numpy as np
 print("TensorFlow version:", tf.__version__)
 print("GPUs:", tf.config.list_physical_devices('GPU'))
+print("NumPy version:", np.__version__)
 
 # print(tensorflow.__file__)
